d6e6e04c43282e809b468c242b434a450c4c1b9d
Wouter Deconinck <wdconinc@gmail.com>
2025-11-01 09:44:28 -0500

    Add Python bindings for Database class

---
+++ python/bindings.cpp
@@ -0,0 +1,8 @@
+#include <pybind11/pybind11.h>
+#include "mollerdb/Database.h"
+namespace py = pybind11;
+PYBIND11_MODULE(mollerdb, m) {
+    py::class_<moller::db::Database>(m, "Database")
+        .def(py::init<const std::string&>())
+        .def("get_main_detector_results", &moller::db::Database::get_main_detector_results_arrow);
+}
